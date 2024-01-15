edc9e175640f026596ea73503dd52541e75c39eb
sachin <132982389+sachinceed@users.noreply.github.com>
2024-01-15 11:12:35 +0530

    Update PSU Owned.py

---
+++ streamlit_app/pages/PSU Owned.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+import geopandas as gpd
+import plotly.graph_objects as go
+import folium
+import plotly.express as px
+from folium.plugins import MarkerCluster
+from streamlit_folium import folium_static
+import json
+
+
+st.subheader('ROOFTOP SOLAR INSTALLED BY DVC')
+
+with open(r'streamlit_app/data/jhnew.geojson') as f:
+    geojson_data = json.load(f)
+
+dfall = pd.read_csv(r'streamlit_app/data/All_Total.csv')
+
+
+# Function to customize tooltip
+def style_function(feature):
+    return {
+        'fillColor': '#10ef3b',
+        'color': 'white',
+        'weight':2,
+        'dashArray': '4, 4',
+        'fillOpacity': 0.7,
+    }
+m = folium.Map(location=[23.6345, 85.3803], zoom_start=7, tiles='cartodb dark_matter',min_zoom =7,max_zoom=7)
+folium.GeoJson(
+    geojson_data,
+    style_function=style_function,
+    tooltip=folium.GeoJsonTooltip(fields=['dtname','DVC_owned_Solar(kwp)'], aliases=['District:','INSTALLED CAPACITY (kwp) BY DVC'])
+).add_to(m)
+folium.LayerControl().add_to(m)
+folium_static(m,width=600,height=400)
+
