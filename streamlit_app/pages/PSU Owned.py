66622a160d37fa0a9d2714db4fc5c3c65e7faf79
sachin <132982389+sachinceed@users.noreply.github.com>
2024-01-15 12:59:51 +0530

    Update PSU Owned.py

--- streamlit_app/pages/PSU Owned.py
+++ streamlit_app/pages/PSU Owned.py
@@ -51,7 +51,7 @@ def style_function(feature):
     }
 
 # Create a color scale
-color_scale = linear.PuBu_09.scale.scale(dfall['DVC_owned_Solar(kwp)'].min(), dfall['DVC_owned_Solar(kwp)'].max())
+color_scale = linear.BuGn_09.scale(dfall['DVC_owned_Solar(kwp)'].min(), dfall['DVC_owned_Solar(kwp)'].max())
 
 # Create the map
 m = folium.Map(location=[23.6345, 85.3803], zoom_start=7, tiles='cartodb dark_matter', min_zoom=7, max_zoom=7,zoomControl=False)
