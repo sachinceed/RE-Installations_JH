4613966a445773133bd382e65dc4039df07150a5
sachin <132982389+sachinceed@users.noreply.github.com>
2024-01-15 11:28:00 +0530

    Update PSU Owned.py

--- streamlit_app/pages/PSU Owned.py
+++ streamlit_app/pages/PSU Owned.py
@@ -35,3 +35,45 @@ folium.GeoJson(
 folium.LayerControl().add_to(m)
 folium_static(m,width=600,height=400)
 
+
+
+
+
+# Function to customize tooltip
+def style_function(feature):
+    return {
+        'fillColor': color_scale(feature['properties']['DVC_solar']),
+        'color': 'white',
+        'weight': 2,
+        'dashArray': '4, 4',
+        'fillOpacity': 0.7,
+    }
+
+# Create a color scale
+color_scale = linear.YlGnBu_09.scale(dfall['DVC_solar'].min(), dfall['DVC_solar'].max())
+
+# Create the map
+m = folium.Map(location=[23.6345, 85.3803], zoom_start=7, tiles='cartodb dark_matter', min_zoom=7, max_zoom=7)
+
+# Add GeoJson layer with customized style and tooltip
+folium.GeoJson(
+    geojson_data,
+    style_function=style_function,
+    tooltip=folium.GeoJsonTooltip(fields=['dtname', 'DVC_solar'], aliases=['District:', 'INSTALLED CAPACITY (kwp) BY DVC'])
+).add_to(m)
+
+# Add color scale legend
+color_scale.caption = 'Installed Capacity (kwp)'
+color_scale.add_to(m)
+
+# Add LayerControl
+folium.LayerControl().add_to(m)
+
+# Display the map
+folium_static(m, width=800, height=600)
+
+
+
+
+
+
