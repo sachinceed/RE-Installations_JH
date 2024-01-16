98466b145f3835681ba9f00c6e27aa44e37ffeaa
sachin <132982389+sachinceed@users.noreply.github.com>
2024-01-16 14:37:03 +0530

    Update State Owned.py

--- streamlit_app/pages/State Owned.py
+++ streamlit_app/pages/State Owned.py
@@ -106,7 +106,7 @@ m = folium.Map(location=[23.6345, 85.3803], zoom_start=7, tiles='CartoDB Positro
 folium.GeoJson(
     geojson_data,
     style_function=style_function,
-    tooltip=folium.GeoJsonTooltip(fields=['dtname','State_SolarGrid_mini_plant','State_solarMinigrid_Plant_capacity'], aliases=['District:','Solar Minigrids count:','Solar Minigrids capacity(kWp):'])
+    tooltip=folium.GeoJsonTooltip(fields=['dtname','State_SolarGrid_mini_plant','State_solarMinigrid_Plant_capacity'], aliases=['District:','Solar Minigrids Count:','Solar Minigrids Capacity(kWp):'])
 ).add_to(m)
 folium.LayerControl().add_to(m)
 folium_static(m,width=600,height=400)
@@ -143,7 +143,7 @@ m = folium.Map(location=[23.6345, 85.3803], zoom_start=7, tiles='CartoDB Positro
 folium.GeoJson(
     geojson_data,
     style_function=style_function,
-    tooltip=folium.GeoJsonTooltip(fields=['dtname','Solar_streetlights_Count'], aliases=['District:','Solar Street lights Count:'])
+    tooltip=folium.GeoJsonTooltip(fields=['dtname','Solar_streetlights_Count'], aliases=['District:','Solar Street Lights Count:'])
 ).add_to(m)
 folium.LayerControl().add_to(m)
 folium_static(m,width=600,height=400)
