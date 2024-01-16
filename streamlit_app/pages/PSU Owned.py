add85fffc9b9f21286ff572f511ca54f8fc8730f
sachin <132982389+sachinceed@users.noreply.github.com>
2024-01-16 12:00:38 +0530

    Update PSU Owned.py

--- streamlit_app/pages/PSU Owned.py
+++ streamlit_app/pages/PSU Owned.py
@@ -17,42 +17,18 @@ with open(r'streamlit_app/data/jhnew.geojson') as f:
     geojson_data = json.load(f)
 
 dfall = pd.read_csv(r'streamlit_app/data/All_Total.csv')
-
-
-# Function to customize tooltip
-def style_function(feature):
-    return {
-        'fillColor': '#10ef3b',
-        'color': 'white',
-        'weight':2,
-        'dashArray': '4, 4',
-        'fillOpacity': 0.7,
-    }
-m = folium.Map(location=[23.6345, 85.3803], zoom_start=7, tiles='cartodb dark_matter',min_zoom =7,max_zoom=7,zoomControl=False)
-folium.GeoJson(
-    geojson_data,
-    style_function=style_function,
-    tooltip=folium.GeoJsonTooltip(fields=['dtname','DVC_solar'], aliases=['District:','INSTALLED CAPACITY (kwp) BY DVC'])
-).add_to(m)
-folium.LayerControl().add_to(m)
-folium_static(m,width=600,height=400)
-
-
-st.subheader('colour Gradient Map')
-
-
 # Function to customize tooltip
 def style_function(feature):
     return {
         'fillColor': color_scale(feature['properties']['DVC_solar']),
-        'color': 'white',
-        'weight': 2,
+        'color': 'black',
+        'weight': 0.7,
         'dashArray': '4, 4',
-        'fillOpacity': 0.7,
+        'fillOpacity': 3.0,
     }
 
 # Create a color scale
-color_scale = linear.BrBG_11.scale(dfall['DVC_owned_Solar(kwp)'].min(), dfall['DVC_owned_Solar(kwp)'].max())
+color_scale = linear.Greens_09.scale(dfall['DVC_owned_Solar(kwp)'].min(), dfall['DVC_owned_Solar(kwp)'].max())
 
 # Create the map
 m = folium.Map(location=[23.6345, 85.3803], zoom_start=7, tiles='cartodb dark_matter', min_zoom=7, max_zoom=7,zoomControl=False)
